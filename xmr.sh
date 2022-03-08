b903b23ba5941d1a1c519c3b8f6d551f9da377c3
okxu3 <101206439+okxu3@users.noreply.github.com>
2022-03-08 14:38:00 -0300

    Delete xmr.sh

--- xmr.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-clear
-sudo apt install screen
-screen -R xmr
- 
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
-cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 86XDTozazjofUBKWoQtzGQCfNdTczT1sKKkXvWqPKSDMA5H3enB8vzsTgjiFbBUt1dVCvq9acPg4tW9cweTBp4Lm7UetXKJ -k --tls --rig-id GC-US
