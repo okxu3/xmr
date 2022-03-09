c8584aeda8987cdda4d2e53b7a29bb16f268f056
okxu3 <101206439+okxu3@users.noreply.github.com>
2022-03-09 11:07:10 -0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,3 +1,11 @@
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+cd xmrig-6.16.4
 {
     "autosave": true,
     "donate-level": 1,
